218ee0426a27ad07445aeeefb0a3ed8b042964a4
Sanchez <ingabuga@yandex.ru>
2022-10-30 17:47:55 +0600

    add request

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -4,8 +4,13 @@ function checkResponse(res) {
     return res.ok ? res.json() : Promise.reject(res.status);
 }
 
+function request(url, options) {
+    // принимает два аргумента: урл и объект опций
+    return fetch(url, options).then(checkResponse)
+}
+
 export const register = (password, email) => {
-    return fetch(`${BASE_URL}/signup`, {
+    return request(`${BASE_URL}/signup`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json" 
@@ -15,11 +20,11 @@ export const register = (password, email) => {
             email
             })
     })
-        .then(response => checkResponse(response))
+        // .then(response => checkResponse(response))
 }
 
 export const login = (password, email) => {
-    return fetch(`${BASE_URL}/signin`, {
+    return request(`${BASE_URL}/signin`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json" 
@@ -29,7 +34,7 @@ export const login = (password, email) => {
             email
             })
     })
-        .then(response => checkResponse(response))
+        // .then(response => checkResponse(response))
         .then(data => { 
             if(data.token) {
                 localStorage.setItem('token', data.token);
@@ -41,13 +46,15 @@ export const login = (password, email) => {
 }
 
 export const checkToken = (token) => {
-    return fetch(`${BASE_URL}/users/me`, {
+    return request(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
             "Content-Type": "application/json",
             "Authorization" : `Bearer ${token}`
             },
     })
-        .then(response => checkResponse(response))
+        // .then(response => checkResponse(response))
         .then(res => res.data)
 }
+
+
